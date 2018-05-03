532efacf6f1d21c1bc4f32d2ca76836d2bac6f69
RamonMJ <ramonmartinezjimenez@gmail.com>
2018-05-03 16:31:01 +0200

    Segunda version de archivos usados en dockerfile
    Modificado los rchivos client_gae_mlhe_mame.sh y server_gae_mlhe_mame.sh con las rutas correctas.

--- server_gaw_mlhe_mame.sh
+++ server_gaw_mlhe_mame.sh
@@ -1,2 +1,2 @@
-cd /home/ramon/Escritorio/TFG/gaminganywhere/bin
+cd ~/proyect/gaminganywhere/bin
 LD_LIBRARY_PATH=../../MediaProcessors_selfcontained/_install_dir_x86/lib ./ga-server-periodic ./config/server.mlhe.mame.conf
