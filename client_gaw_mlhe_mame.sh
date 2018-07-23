7e8b0ff622f6513798719fef2a10251f3b4b14cb
RamonMJ <ramonmartinezjimenez@gmail.com>
2018-07-23 04:02:54 +0200

    Tercera version de los archivos - 23/07/2018

--- client_gaw_mlhe_mame.sh
+++ client_gaw_mlhe_mame.sh
@@ -1,2 +1,2 @@
-cd ~/proyect/gaminganywhere/bin
+cd /proyect/gaminganywhere/bin
 LD_LIBRARY_PATH=../../MediaProcessors_selfcontained/_install_dir_x86/lib ./ga-client ./config/client.mlhe.abs.conf rtsp://127.0.0.1:8554/desktop
